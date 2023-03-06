8e2e97b4f85a9a87335cd53d8a54c651445b8dad
Herzeg.uz <110915492+Herzeg137@users.noreply.github.com>
2023-03-06 17:33:32 +0500

    Create 977.py

---
+++ 977.py
@@ -0,0 +1,7 @@
+class Solution(object):
+    def sortedSquares(self, nums):
+      #1 way => Time: O(n + nlogn), space: O(1)
+      return sorted((map(lambda x: pow(x, 2), nums)) 
+      
+      
+ #Link to problem: https://leetcode.com/problems/squares-of-a-sorted-array/description/
